dac873df2b7050160c28888612509e3b8389f0cc
Donald Vallejo <donny.vallejo@gmail.com>
2019-09-21 22:04:02 -0700

    trying over again

---
+++ heros.py
@@ -0,0 +1,15 @@
+class hero:
+    def __init__(self, name, starting_health):
+        self.name = name
+        self.add_ability = add_ability
+
+
+class Ability:
+    def __init__(self, name, attack_strength):        
+        self.attack = attack
+
+class Armor:    
+    def __init__(self,name, block):        
+        self.block = block
+
+add_ability = add_ability
---
+++ superheroes.py
@@ -0,0 +1,16 @@
+# import heros
+import random
+
+class Ability:
+    def __init__(self, name, attack_strength):        
+        self.name = name
+        self.attack_strength = attack_strength
+        # self.max_damage = 200
+        
+    def attack(self):
+        
+        return
+
+hero1 = Ability("Duff Man", 50)
+hero1.max_damage = 200
+print(hero1)
