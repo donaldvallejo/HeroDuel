4cc576a23721eff2fd34091c9749419c63320ed5
Donald Vallejo <donny.vallejo@gmail.com>
2019-09-25 14:40:45 -0700

    almsot done with page 2

--- superheroes.py
+++ superheroes.py
@@ -25,7 +25,7 @@ class Hero:
         self.abilities = []
         self.armors = []   
         self.starting_health = starting_health
-        # self.current_health = 0
+        self.current_health = 0
         
     def add_ability(self, ability):
         self.abilities.append(ability)
@@ -33,20 +33,30 @@ class Hero:
     def add_armor(self, armor):
         self.armors.append(armor)
 
+    def defend(self, damage_amt):
+        # self.block(damage_amt)
+        block_total = 0
+        for defend in self.armors:
+            block_strength = defend.block()
+            block_total += block_strength
+        return block_total
+
+    """ double check the values of this function doc says output should be between 150 and 200 """
+    def take_damage(self, damage):
+        self.current_health = self.attack() + self.defend(damage)
+        print(f"current health is {self.current_health}")
+
     def attack(self):
         attack_total = 0
         for ability in self.abilities:
             attack_strength = ability.attack()
             attack_total += attack_strength 
+        print(f"total attack damage : {attack_total}")
         return attack_total
 
-    # def defend(self):
-    #     pass
-
-
-    # def take_damage(self):
-    #     pass
-
+    def is_alive(self):  
+   
+        pass
 
 def create_duff_man():
     """ Hero object"""
@@ -60,10 +70,13 @@ def create_duff_man():
 
     """ Armor instance objects"""
     block = Armor("block!", 50)
+    shield = Armor("shield block!", 70)
     hero.add_armor(block)
+    hero.add_armor(shield)
+    hero.defend(2)
+    print(f"Defended {hero.defend(2)} points of damage")
 
     return hero
-
 
 def create_grace_hopper():
     hero = Hero("Grace Hopper", 200)
@@ -79,13 +92,16 @@ def create_grace_hopper():
 def duff_man_battles_grace_hopper():
     duff_man = create_duff_man()
     grace = create_grace_hopper()
-    for attack_number in range(5):
+    for attack_number in range(1):
         duff_attack_power = duff_man.attack()
         grace_attack_power = grace.attack()
         print("Duff Man's attack:", duff_attack_power)
         print("Grace's attack:", grace_attack_power)
 
-        """ Deal damage to each hero """
+if __name__ == "__main__":
+    duff_man_battles_grace_hopper()
+
+    """ Deal damage to each hero """
         # duff_man.take_damage(grace_attack_power)
         # grace.take_damage(duff_attack_power)
 
@@ -96,7 +112,18 @@ def duff_man_battles_grace_hopper():
         #     print()
 
 if __name__ == "__main__":
-    duff_man_battles_grace_hopper()
+    hero = Hero("Grace Hopper", 200)
+    hero.take_damage(150)
+    print(hero.is_alive())
+    hero.take_damage(15000)
+    print(hero.is_alive())
+
+# if __name__ == "__main__":
+#     hero = Hero("Grace Hopper", 200)
+#     shield = Armor("Shield", 50)
+#     hero.add_armor(shield)
+#     hero.take_damage(50)
+#     print(f"current health is {hero.current_health}")
 
 # if __name__ == "__main__":
 #     ability = Ability("Great Debugging", 50)
